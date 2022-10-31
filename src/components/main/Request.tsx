743efb43c549ec32994d73ab26e5f5aff2d8b9d9
nguyen-hoang-nam-engineer <nguyenhoangnam@engineer.com>
2022-10-31 22:32:56 +0700

    Feat: Add tab in request

--- src/components/main/Request.tsx
+++ src/components/main/Request.tsx
@@ -1,4 +1,4 @@
-import { Component, createSignal, Setter } from "solid-js";
+import { Component, createSignal, Match, Setter, Switch } from "solid-js";
 
 import type { JSX } from "solid-js";
 
@@ -7,27 +7,93 @@ const Request: Component<{
     headers: Object;
     setRequest: Setter<Object>;
 }> = (props) => {
-    const [value, setValue] = createSignal(props.body);
+    const [bodyValue, setBodyValue] = createSignal(props.body);
+    const [headersValue, setHeadersValue] = createSignal(props.headers);
+    const [tab, setTab] = createSignal("body");
 
-    const onChange: JSX.EventHandler<HTMLTextAreaElement, Event> = (event) => {
+    const onChangeBody: JSX.EventHandler<HTMLTextAreaElement, Event> = (
+        event
+    ) => {
         props.setRequest((request: object) => {
             request["body"] = event.currentTarget.value;
             return request;
         });
 
-        setValue(event.currentTarget.value);
+        setBodyValue(event.currentTarget.value);
+    };
+
+    const onChangeHeaders: JSX.EventHandler<HTMLTextAreaElement, Event> = (
+        event
+    ) => {
+        props.setRequest((request: object) => {
+            try {
+                request["headers"] = JSON.parse(event.currentTarget.value);
+            } catch (e) {
+                request["headers"] = {};
+            }
+
+            return request;
+        });
+
+        try {
+            setHeadersValue(JSON.parse(event.currentTarget.value));
+        } catch (e) {
+            setHeadersValue("");
+        }
+    };
+
+    const updateTab = (value: string) => {
+        setTab(value);
     };
 
     return (
-        <div>
-            <textarea
-                class="bg-white p-3 box-border resize-none"
-                style="height: 100%; width: 100%; outline: none;"
-                value={value()}
-                onChange={onChange}
+        <div class="flex flex-col" style="height: 100%; ">
+            <ul
+                class="flex gap-4"
+                style="list-style-type: none; padding: 0; flex-shrink: 0;"
             >
-                {value()}
-            </textarea>
+                <li
+                    class="cursor-pointer"
+                    onClick={() => {
+                        updateTab("headers");
+                    }}
+                >
+                    Headers
+                </li>
+
+                <li
+                    class="cursor-pointer"
+                    onClick={() => {
+                        updateTab("body");
+                    }}
+                >
+                    Body
+                </li>
+            </ul>
+
+            <Switch>
+                <Match when={tab() === "body"}>
+                    <textarea
+                        class="bg-white p-3 box-border resize-none"
+                        style="width: 100%; outline: none; flex-grow: 1;"
+                        value={bodyValue()}
+                        onChange={onChangeBody}
+                    >
+                        {bodyValue()}
+                    </textarea>
+                </Match>
+
+                <Match when={tab() === "headers"}>
+                    <textarea
+                        class="bg-white p-3 box-border resize-none"
+                        style="width: 100%; outline: none; flex-grow: 1;"
+                        value={JSON.stringify(headersValue())}
+                        onChange={onChangeHeaders}
+                    >
+                        {JSON.stringify(headersValue())}
+                    </textarea>
+                </Match>
+            </Switch>
         </div>
     );
 };
